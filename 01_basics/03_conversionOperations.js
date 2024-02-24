5d54bd480cf7414e9cc552d9a4e833d46509016b
AyushDev2002 <153273045+AyushDev2002@users.noreply.github.com>
2024-02-24 11:21:21 +0000

    conversionOperations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,4 +1,4 @@
-let score = "33abc"
+/* let score = "33abc"
 
 console.log(typeof score);
 console.log(typeof (score))
@@ -28,3 +28,32 @@ let someNumber = 33;
 let stringNumber = String(someNumber);
 
 console.log(stringNumber);
+
+*/
+// ******************** Operations *********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+/* console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**2)
+console.log(2/2)
+console.log(2%2) */
+
+let str1 = "hello"
+let str2 = " Ayush"
+
+let str3 = str1 + str2
+
+console.log(str3);
+
+console.log(1+"2");
+console.log("1"+2);
+console.log("1"+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+
