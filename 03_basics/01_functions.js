f1f1cee227ba3fb2c3b9048741796db949655ecd
AyushDev2002 <153273045+AyushDev2002@users.noreply.github.com>
2024-02-27 09:04:15 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -41,7 +41,31 @@ function loginUserMessage(username){
     return `${username} just logged in`
 }
 
-console.log(loginUserMessage())
+// console.log(loginUserMessage())
 
 
+function calculateCartPrice(...num1){
+    return num1
+}
 
+// console.log(calculateCartPrice(2));
+// console.log(calculateCartPrice(200, 500, 600));   // return only a single value to do all include ... in declaration
+
+const user = {
+    username: "Ayush",
+    price: 100
+}
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+handleObject(user)
+
+const myArray = [200, 300, 400, 500]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myArray));
