0c5bc65e51bfda85dbd6e126d48ba96b03492ca1
AyushDev2002 <153273045+AyushDev2002@users.noreply.github.com>
2024-02-27 09:46:19 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -4,9 +4,50 @@ let a = 300
 if(true){
     let a = 10
     const b = 20
-    console.log("Inner: ", a);
+    // console.log("Inner: ", a);
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function one(){
+    const username = "Ayush"
+
+    function two(){
+        const website = "Youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+}
+
+// one()
+
+if(true){
+    const username = "Ayush"
+    if (username === "Ayush") {
+        const website = " Youtube"
+        // console.log(username + website)
+    }
+    // console.log(website);
+}
+
+// console.log(username)
+
+
+//++++++++++++++++++++++++++++ INTERESTING ++++++++++++++++++++++++++++
+
+console.log(addone(5))
+
+function addone(num){
+    return num+1
+}
+
+addTwo(5)                                   // it cannot be accessed because now we just dont declare a function instead we put it in a variable also
+const addTwo = function(num){
+    return num+2
+}
+
